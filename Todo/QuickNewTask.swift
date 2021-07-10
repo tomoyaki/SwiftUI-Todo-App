3de4e52cec23231ba1a07f5fefcfc3ed3185b7a8
tomoyaki <tk5819555@gmail.com>
2021-07-10 16:39:52 +0900

    add text field

---
+++ Todo/QuickNewTask.swift
@@ -0,0 +1,41 @@
+//
+//  QuickNewTask.swift
+//  Todo
+//
+//  Created by Tomoya Kitazawa on 2021/07/10.
+//
+
+import SwiftUI
+
+struct QuickNewTask: View {
+    var category: TodoEntity.Category
+    
+    @State var newTask: String = ""
+    
+    fileprivate func addNewTask(){
+        self.newTask = ""
+    }
+    
+    fileprivate func cancelTask(){
+        self.newTask = ""
+    }
+    
+    var body: some View {
+        HStack {
+            TextField("新しいタスク", text: $newTask)
+            Button(action: {self.addNewTask()}) {
+                Text("追加")
+            }
+            Button(action: {self.cancelTask()}) {
+                Text("Cancel")
+                    .foregroundColor(.red)
+            }
+        }
+    }
+}
+
+struct QuickNewTask_Previews: PreviewProvider {
+    static var previews: some View {
+        QuickNewTask(category: .ImpUrg_1st)
+    }
+}
