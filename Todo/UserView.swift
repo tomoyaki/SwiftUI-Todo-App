8443ee2715f86562e7dc512cc2191eff904f8b79
tomoyaki <tk5819555@gmail.com>
2021-07-10 14:29:30 +0900

    change user view color

--- Todo/UserView.swift
+++ Todo/UserView.swift
@@ -12,17 +12,25 @@ struct UserView: View {
         HStack {
             VStack(alignment: .leading) {
                 Text("こんにちは")
+                    .foregroundColor(Color.tTitle)
                 Text("tomoyaki")
+                    .foregroundColor(Color.tTitle)
             }
+            Spacer()
             Image("profile")
                 .resizable()
                 .frame(width: 60, height: 60)
-        }
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+        }.padding()
+        .background(Color.tBackground)
     }
 }
 
 struct UserView_Previews: PreviewProvider {
     static var previews: some View {
-        UserView()
+        Group{
+            UserView()
+            Circle()
+        }
     }
 }
